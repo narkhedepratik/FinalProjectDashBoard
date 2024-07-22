958845c601aa639a5e7ee7218b2c682ffdfc0f71
narkhedepratik <pratiknarkhede039@gmail.com>
2024-07-22 10:57:08 +0530

    project dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisitorsComponent } from './template/visitors/visitors.component';
+import { HomeComponent } from './template/home/home.component';
+import { EmiCalculatorComponent } from './include/emi-calculator/emi-calculator.component';
+import { EnquiryComponent } from './include/enquiry/enquiry.component';
+import { SignInComponent } from './include/sign-in/sign-in.component';
+import { UsersDashboardComponent } from './include/users-dashboard/users-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" ,redirectTo:"SukhadLoan" ,pathMatch:'full'},
+  {path:'SukhadLoan' , component: VisitorsComponent , children:[
+    {path:'home',component:HomeComponent},
+    {path:'emi', component:EmiCalculatorComponent},
+    {path:"enquiry",component:EnquiryComponent},
+    {path:"sign-in",component:SignInComponent}
+
+  ]},
+  {path:'dash',component:UsersDashboardComponent ,children:[
+    {path:'ADMIN', loadChildren:()=>import('./modules/admin/admin.module').then(file=>file.AdminModule)},
+    {path:'CRM',   loadChildren:()=>import('./modules/crm/crm.module').then(file=>file.CrmModule)}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VisitorsComponent } from './template/visitors/visitors.component';
+import { UsersDashboardComponent } from './include/users-dashboard/users-dashboard.component';
+import { HomeComponent } from './template/home/home.component';
+import { EmiCalculatorComponent } from './include/emi-calculator/emi-calculator.component';
+import { EnquiryComponent } from './include/enquiry/enquiry.component';
+import { SignInComponent } from './include/sign-in/sign-in.component';
+import { VisitorHeaderComponent } from './include/visitor-header/visitor-header.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UserHeaderComponent } from './include/user-header/user-header.component';
+import { SideNavComponent } from './include/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisitorsComponent,
+    UsersDashboardComponent,
+    HomeComponent,
+    EmiCalculatorComponent,
+    EnquiryComponent,
+    SignInComponent,
+    VisitorHeaderComponent,
+    UserHeaderComponent,
+    SideNavComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
